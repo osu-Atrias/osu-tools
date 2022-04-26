c19433afdd878516e9d26fd059993c66aad79ca5
Wtz_LASR <wtz_lasr@outlook.com>
2022-04-26 23:51:08 +0800

    Revert "upstream修不好可以不修"
    This reverts commit 5c367b1cea78886a6f432e4699f6f941b877edb9.

--- PerformanceCalculator/Simulate/SimulateCommand.cs
+++ PerformanceCalculator/Simulate/SimulateCommand.cs
@@ -67,10 +67,9 @@ namespace PerformanceCalculator.Simulate
         {
             var ruleset = Ruleset;
 
-            var mods = GetMods(ruleset);
-            var difficultyAdjustmentMods = NoClassicMod ? mods : LegacyHelper.ConvertToLegacyDifficultyAdjustmentMods(ruleset, mods);
+            var mods = NoClassicMod ? GetMods(ruleset) : LegacyHelper.ConvertToLegacyDifficultyAdjustmentMods(ruleset, GetMods(ruleset));
             var workingBeatmap = ProcessorWorkingBeatmap.FromFileOrId(Beatmap);
-            var beatmap = workingBeatmap.GetPlayableBeatmap(ruleset.RulesetInfo, difficultyAdjustmentMods);
+            var beatmap = workingBeatmap.GetPlayableBeatmap(ruleset.RulesetInfo, mods);
 
             var beatmapMaxCombo = GetMaxCombo(beatmap);
             var maxCombo = Combo ?? (int)Math.Round(PercentCombo / 100 * beatmapMaxCombo);
@@ -79,7 +78,7 @@ namespace PerformanceCalculator.Simulate
             var accuracy = GetAccuracy(statistics);
 
             var difficultyCalculator = ruleset.CreateDifficultyCalculator(workingBeatmap);
-            var difficultyAttributes = difficultyCalculator.Calculate(difficultyAdjustmentMods);
+            var difficultyAttributes = difficultyCalculator.Calculate(mods);
             var performanceCalculator = ruleset.CreatePerformanceCalculator();
 
             var ppAttributes = performanceCalculator?.Calculate(new ScoreInfo(beatmap.BeatmapInfo, ruleset.RulesetInfo)
